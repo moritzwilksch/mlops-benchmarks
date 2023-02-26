c478d7921e6fe62b3c0be30ea1f1a4d7442e3444
Moritz Wilksch <moritzwilksch@gmail.com>
2023-02-26 16:19:26 +0000

    wip: setup onnx runtime

--- src/inference_api/src/main.rs
+++ src/inference_api/src/main.rs
@@ -1,9 +1,11 @@
-use warp::Filter;
+use ndarray::prelude::*;
+use onnxruntime::{environment::Environment, ndarray, GraphOptimizationLevel, LoggingLevel};
+use std::array;
 
 fn sum_vals(a: i64, b: i64) -> String {
     let sum = a + b;
     println!("{sum}");
-    return format!("{sum}")
+    return format!("{sum}");
 }
 
 #[tokio::main]
@@ -11,5 +13,29 @@ async fn main() {
     // let hello = warp::path!("hello" / i64 / i64).map(sum_vals);
     // warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 
-    
+    let environment = Environment::builder()
+        .with_name("test")
+        .with_log_level(LoggingLevel::Verbose)
+        .build()
+        .unwrap();
+
+    let session = environment
+        .new_session_builder()
+        .unwrap() // alternative?
+        .with_optimization_level(GraphOptimizationLevel::Basic)
+        .unwrap()
+        .with_number_threads(1)
+        .unwrap()
+        .with_model_from_file("../../artifacts/model.onnx")
+        .unwrap();
+
+    // TBD
+    let input = vec![
+        array![1.0],
+        array![String::from("Female")],
+        array![String::from("No")],
+        array![String::from("Sun")],
+        array![String::from("Dinner")],
+        array![2],
+    ];
 }
